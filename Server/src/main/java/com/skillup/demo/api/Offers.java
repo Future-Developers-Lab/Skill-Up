495443fe10e0c9eda8bb9a72ccb2a975d8401770
Jakub <jradzik4@gmail.com>
2021-09-26 21:28:44 +0200

    Server Init

---
+++ Server/src/main/java/com/skillup/demo/api/Offers.java
@@ -0,0 +1,31 @@
+package com.skillup.demo.api;
+
+import com.skillup.demo.dao.OfferDAO;
+import com.skillup.demo.services.OffersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Offers {
+
+    private final OffersService offersService;
+
+    @Autowired
+    public Offers(OffersService offersService) {
+        this.offersService = offersService;
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    String helloWorld() {
+        return "Hello World";
+    }
+
+    @RequestMapping(path = "/offers", method = RequestMethod.GET)
+    List<OfferDAO> getAllOffers(){
+        return offersService.getAllOffers();
+    }
+}
---
+++ Server/src/main/java/com/skillup/demo/services/OffersService.java
@@ -0,0 +1,22 @@
+package com.skillup.demo.services;
+
+import com.skillup.demo.dao.OfferDAO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OffersService {
+
+    public List<OfferDAO> getAllOffers() {
+        return List.of(
+                new OfferDAO("Title 1", "Description 1"),
+                new OfferDAO("Title 2", "Description 2"),
+                new OfferDAO("Title 3", "Description 3"),
+                new OfferDAO("Title 4", "Description 4"),
+                new OfferDAO("Title 5", "Description 5"),
+                new OfferDAO("Title 6", "Description 6"),
+                new OfferDAO("Title 7", "Description 7")
+        );
+    }
+}
